303e9e1491f4830b1cee0a071660139438e9201e
ibnu maulana <45716669+ibnumaulana215@users.noreply.github.com>
2018-12-16 19:00:15 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i,j;
+    for (i=1; i<=10; i++){
+        for (j=1; j<=5; j++){
+            printf("i= %d,j= %d\t",i,j);
+        }
+    }
+
+}
